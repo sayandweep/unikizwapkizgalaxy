362ecb1bde2d5c7dd328d5ddeeced0cd47b83113
sayandweep <81100142+sayandweep@users.noreply.github.com>
2021-07-03 20:19:44 +0530

    Update whatsappplugin.js

--- whatsappplugin/whatsappplugin.js
+++ whatsappplugin/whatsappplugin.js
@@ -1,1 +1,2 @@
-A
+var name1; var txt = "unikizwapkizgalaxy"; name1=("Whatsapp<br><small>unikizwapkizgalaxy</small>"); document.write('<p class="style1">'+name1+'</p>'); //A Free Plugin by UnikizWapkizGalaxy;
+

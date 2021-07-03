960003bdb75cae89c3cf772cc60e3a230faa3aca
sayandweep <81100142+sayandweep@users.noreply.github.com>
2021-07-03 20:10:06 +0530

    Create whatsappplugin.js

---
+++ whatsappplugin.js
@@ -0,0 +1,1 @@
+var name1; var txt = "unikizwapkizgalaxy"; name1=("Whatsapp<br><small>unikizwapkizgalaxy</small>"); document.write('<p class="style1">'+name1+'</p>'); //A Free Plugin by UnikizWapkizGalaxy;

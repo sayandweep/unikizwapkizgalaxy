48948e1cdb729068eef62c4eeeae8d467defd4c4
sayandweep <81100142+sayandweep@users.noreply.github.com>
2021-07-16 22:41:58 +0530

    Update

--- copyright.js
+++ copyright.js
@@ -1,8 +1,8 @@
 // A Product by UnikizWapkizGalaxy
 
-// Noticeboard : To Download products from our station, you must accept our Terms & Conditions. You can't sell it without our permission. You can't make changes on it without permission. You can't sell it telling your products. 
+/* Noticeboard : To Download products from our station, you must accept our Terms & Conditions. You can't sell it without our permission. You can't make changes on it without permission. You can't sell it telling your products. 
 What if you don't obey : We will block your ip address. You then can't access our website. Your ip automatically open after 4 months. But if you disobey us for 2nd time then you will be permanently blocked.
-Become Our Member : Become our member. You can easily come with us by showing your talent on wapkiz or wapaxo. We also welcome php developers and wapka and waphot developers;
+Become Our Member : Become our member. You can easily come with us by showing your talent on wapkiz or wapaxo. We also welcome php developers and wapka and waphot developers */
 What can you get by joining us : You can access Premium content for free. You can develop that. You can earn from it. And get points for your work. 
 
 // Facebook - UnikizWapkizGalaxy || Telegram - UnikizWapkizGalaxy || YouTube - UnikizWapkizGalaxy
